6e8e4a01013242a435bc2ef470446b1c2e4531f7
Geva <geva93@gmail.com>
2020-05-31 21:51:32 +0300

    Done with all screens. Next step is to enable state management via Redux

--- App.js
+++ App.js
@@ -1,32 +1,31 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 import * as Font from 'expo-font';
 import { AppLoading } from 'expo';
 import { enableScreens } from 'react-native-screens';
 
 import MealsNavigator from './navigation/MealsNavigator';
+
 enableScreens();
+
 const fetchFonts = () => {
-  Font.loadAsync({
+  return Font.loadAsync({
     'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
     'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
-  })
-}
-export default function App() {
+  });
+};
 
-  const [fontLoaded, setFondLoaded] = useState(false);
+export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
   if (!fontLoaded) {
     return (
       <AppLoading
         startAsync={fetchFonts}
-        onFinish={() => setFondLoaded(true)}
+        onFinish={() => setFontLoaded(true)}
       />
-    )
+    );
   }
-  return (
-    <MealsNavigator />
-  );
-}
 
-const styles = StyleSheet.create({
-});
+  return <MealsNavigator />;
+}
